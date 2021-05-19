74741682ad547913d8f277444af787fccba592b9
chizhangucb <chi.zhang@databricks.com>
2021-05-20 00:17:44 +0800

    Update Retail Sales Pipeline.py

--- python/Retail Sales/Retail Sales Pipeline.py
+++ python/Retail Sales/Retail Sales Pipeline.py
@@ -4,19 +4,17 @@
 # Data Set Information
 # ====================
 # * Sales Orders: **sales_orders/sales_orders.json** records the customers' originating purchase order.
-# * Purchase Orders: **purchase_orders/purchase_orders.xml** contains the raw materials that are being purchased.
 # * Products: **products/products.csv** contains products that the company sells.
-# * Goods Receipt: **goods_receipt/goods_receipt.parquet** contains the arrival time of purchased orders.
 # * Customers: **customers/customers.csv** contains those customers who are located in the US and are buying the finished products.
-# * Suppliers: **suppliers/suppliers.csv** contains suppliers that provide raw materials in the US.
-# * Sales Stream: **sales_stream/sales_stream.json/** is a folder containing JSON files for streaming purposes.
-# * Promotions: **promotions/promotions.csv** contains additional benefits on top of normal purchases.
-# * Active Promotions: **active_promotions/active_promotions.parquet** shows how customers are progressing towards becoming eligible for promotions.
-# * Loyalty Segment: **loyalty_segment/loyalty_segment.csv**  contains segmented customer data to appeal to all types of guests using targeted rewards and promotions.
 
 # Use the following command to get the full filepath
 #dbutils.notebook.entry_point.getDbutils().notebook().getContext().notebookPath().get() 
 
+#
+# Import 
+#
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
 
 @create_view(name="products_master")
 def products_master():
