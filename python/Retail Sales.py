30a7a58006b0fc5f3653d6a4343e3b7bafbde29e
Chris Fish <chrishoshinofish@gmail.com>
2021-06-21 17:06:12 -0700

    Update Python examples to include dlt module

--- python/Retail Sales.py
+++ python/Retail Sales.py
@@ -1,4 +1,3 @@
-# Databricks notebook source
 # 
 # Synthetic Retail Dataset
 # Data Set Information
@@ -9,51 +8,57 @@
 
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
+import dlt
 
-@create_view(
+@dlt.create_view(
   comment="The customers buying finished products, ingested from /databricks-datasets."
 )
 def customers():
   return spark.read.csv('/databricks-datasets/retail-org/customers/customers.csv', header=True)
- 
-
-@create_table(
+  
+@dlt.create_table(
   comment="The raw sales orders, ingested from /databricks-datasets.",
   table_properties={
-    "quality": "bronze"
+    "quality": "bronze",
+    "pipelines.autoOptimize.managed": "true"
   }
 )
 def sales_orders_raw():
-  return spark.read.json('/databricks-datasets/retail-org/sales_orders/part-00000-tid-1771549084454148016-e2275afd-a5bb-40ed-b044-1774c0fdab2b-105592-1-c000.json')
+  return (
+    spark.read.option("inferSchema", "true").json('/databricks-datasets/retail-org/sales_orders/part-00000-tid-1771549084454148016-e2275afd-a5bb-40ed-b044-1774c0fdab2b-105592-1-c000.json')
+  )
 
   
-@create_table(
+@dlt.create_table(
   comment="The cleaned sales orders with valid order_number(s) and partitioned by order_datetime",
   table_properties={
-    "quality": "silver"
-  },
-  partition_cols=["order_datetime"]
+    "quality": "silver",
+    "pipelines.autoOptimize.managed": "true"
+  }
 )
-@expect_or_drop("valid order_number", "order_number IS NOT NULL")
+@dlt.expect_or_drop("valid order_number", "order_number IS NOT NULL")
 def sales_orders_cleaned():
-    return read_stream("sales_orders_raw").join(read("customers"), ["customer_id", "customer_name"], "left")
+    return dlt.read_stream("sales_orders_raw").join(dlt.read("customers"), ["customer_id", "customer_name"], "left")
+
 
   
-@create_table(
+@dlt.create_table(
   comment="Sales orders in LA",
   table_properties={
-    "quality": "gold"
+    "quality": "gold",
+    "pipelines.autoOptimize.managed": "true"
   }
 )
 def sales_order_in_la():
-  return read("sales_orders_cleaned").where("city == 'Los Angeles'")  
+  return dlt.read_stream("sales_orders_cleaned").where("city == 'Los Angeles'")  
 
   
-@create_table(
+@dlt.create_table(
   comment="Sales orders in Chicago",
   table_properties={
-    "quality": "gold"
+    "quality": "gold",
+    "pipelines.autoOptimize.managed": "true"
   }
 )
 def sales_order_in_chicago():
-  return read("sales_orders_cleaned").where("city == 'Chicago'")    
+  return dlt.read_stream("sales_orders_cleaned").where("city == 'Chicago'")
--- python/Wikipedia.py
+++ python/Wikipedia.py
@@ -1,4 +1,3 @@
-# Databricks notebook source
 # 
 # Wikipedia Clickstream
 # An example Delta Live Tables pipeline that ingests wikipedia click stream data and builds some simple summary tables.
@@ -9,10 +8,11 @@
 
 from pyspark.sql.functions import *
 from pyspark.sql.types import *
-
+import dlt
 
 json_path = "/databricks-datasets/wikipedia-datasets/data-001/clickstream/raw-uncompressed-json/2015_2_clickstream.json"
-@create_table(
+
+@dlt.create_table(
   comment="The raw wikipedia click stream dataset, ingested from /databricks-datasets.",
   table_properties={
     "quality": "bronze"
@@ -20,31 +20,31 @@ json_path = "/databricks-datasets/wikipedia-datasets/data-001/clickstream/raw-un
 )
 def clickstream_raw():          
   return (
-    spark.read.json(json_path)
+    spark.read.option("inferSchema", "true").json(json_path)
   )
 
 
-@create_table(
+@dlt.create_table(
   comment="Wikipedia clickstream dataset with cleaned-up datatypes / column names and quality expectations.",
   table_properties={
-    "quality": "silver"  
+    "quality": "silver"
   }
 )
-@expect("valid_current_page", "current_page_id IS NOT NULL AND current_page_title IS NOT NULL")
-@expect_or_fail("valid_count", "click_count > 0")
+@dlt.expect("valid_current_page", "current_page_id IS NOT NULL AND current_page_title IS NOT NULL")
+@dlt.expect_or_fail("valid_count", "click_count > 0")
 def clickstream_clean():
   return (
-    read("clickstream_raw")
+    dlt.read("clickstream_raw")
       .withColumn("current_page_id", expr("CAST(curr_id AS INT)"))
       .withColumn("click_count", expr("CAST(n AS INT)"))
       .withColumn("previous_page_id", expr("CAST(prev_id AS INT)"))
       .withColumnRenamed("curr_title", "current_page_title")
       .withColumnRenamed("prev_title", "previous_page_title")
-      .select("current_page_id", "current_page_title", "click_count", "previous_page_id", "previous_page_title")      
+      .select("current_page_id", "current_page_title", "click_count", "previous_page_id", "previous_page_title")
   )
 
 
-@create_table(
+@dlt.create_table(
   comment="A table of the most common pages that link to the Apache Spark page.",
   table_properties={
     "quality": "gold"  
@@ -52,7 +52,7 @@ def clickstream_clean():
 )
 def top_spark_referrers():
   return (
-    read("clickstream_clean")
+    dlt.read("clickstream_clean")
       .filter(expr("current_page_title == 'Apache_Spark'"))
       .withColumnRenamed("previous_page_title", "referrer")
       .sort(desc("click_count"))
@@ -61,7 +61,7 @@ def top_spark_referrers():
   )
 
 
-@create_table(
+@dlt.create_table(
   comment="A list of the top 50 pages by number of clicks.",
   table_properties={
     "quality": "gold"  
@@ -69,7 +69,7 @@ def top_spark_referrers():
 )
 def top_pages():
   return (
-    read("clickstream_clean")
+    dlt.read("clickstream_clean")
       .groupBy("current_page_title")
       .agg(sum("click_count").alias("total_clicks"))
       .sort(desc("total_clicks"))
