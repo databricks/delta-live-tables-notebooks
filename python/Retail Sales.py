049e67ec33312118568badd56b2e8468f56f4ff1
jodb <jodb>
2021-09-20 23:11:03 +0000

    Updated Retail Sales python notebook
    Made the python pipeline aggregate in the gold tables.
    
    Also updated the quality properties in the SQL notebook

--- python/Retail Sales.py
+++ python/Retail Sales.py
@@ -16,50 +16,80 @@ import dlt
 )
 def customers():
   return spark.read.csv('/databricks-datasets/retail-org/customers/customers.csv', header=True)
-  
+
+
+# COMMAND ----------
+
+# The table property "myCompanyPipeline.quality": "bronze" is a namespaced table property that is used to describe the table quality for later
 @dlt.create_table(
   comment="The raw sales orders, ingested from /databricks-datasets.",
   table_properties={
-    "quality": "bronze",
+    "myCompanyPipeline.quality": "bronze",
     "pipelines.autoOptimize.managed": "true"
   }
 )
 def sales_orders_raw():
   return (
-    spark.read.option("inferSchema", "true").json('/databricks-datasets/retail-org/sales_orders/part-00000-tid-1771549084454148016-e2275afd-a5bb-40ed-b044-1774c0fdab2b-105592-1-c000.json')
+    spark.readStream.format("cloudFiles") \
+      .option("cloudFiles.schemaLocation", "/tmp/john.odwyer/pythonsalestest") \
+      .option("cloudFiles.format", "json") \
+      .option("cloudFiles.inferColumnTypes", "true") \
+      .load("/databricks-datasets/retail-org/sales_orders/")
   )
 
-  
+# COMMAND ----------
+
 @dlt.create_table(
-  comment="The cleaned sales orders with valid order_number(s) and partitioned by order_datetime",
+  comment="The cleaned sales orders with valid order_number(s) and partitioned by order_date",
+  partition_cols=["order_date"],
   table_properties={
-    "quality": "silver",
+    "myCompanyPipeline.quality": "silver",
     "pipelines.autoOptimize.managed": "true"
   }
 )
 @dlt.expect_or_drop("valid order_number", "order_number IS NOT NULL")
 def sales_orders_cleaned():
-    return dlt.read_stream("sales_orders_raw").join(dlt.read("customers"), ["customer_id", "customer_name"], "left")
+  df = dlt.read_stream("sales_orders_raw").join(dlt.read("customers"), ["customer_id", "customer_name"], "left")
+  df = df.withColumn("order_datetime", from_unixtime(df.order_datetime).cast("TIMESTAMP")) 
+  df = df.withColumn("order_date", df.order_datetime.cast("DATE")) 
+  df = df.select("customer_id", "customer_name", "number_of_line_items", "order_datetime", "order_date",
+    "order_number", "ordered_products", "state", "city", "lon", "lat", "units_purchased", "loyalty_segment")
+  return df
 
 
-  
+# COMMAND ----------
+
 @dlt.create_table(
-  comment="Sales orders in LA",
+  comment="Aggregated sales orders in LA",
   table_properties={
-    "quality": "gold",
+    "myCompanyPipeline.quality": "gold",
     "pipelines.autoOptimize.managed": "true"
   }
 )
 def sales_order_in_la():
-  return dlt.read_stream("sales_orders_cleaned").where("city == 'Los Angeles'")  
+  df = dlt.read_stream("sales_orders_cleaned").where("city == 'Los Angeles'") 
+  df = df.select(df.city, df.order_date, df.customer_id, df.customer_name, explode(df.ordered_products).alias("ordered_products_explode"))
 
-  
+  dfAgg = df.groupBy(df.order_date, df.city, df.customer_id, df.customer_name, df.ordered_products_explode.curr.alias("currency"))\
+    .agg(sum(df.ordered_products_explode.price).alias("sales"), sum(df.ordered_products_explode.qty).alias("qantity"))
+
+  return dfAgg
+
+
+# COMMAND ----------
+
 @dlt.create_table(
   comment="Sales orders in Chicago",
   table_properties={
-    "quality": "gold",
+    "myCompanyPipeline.quality": "gold",
     "pipelines.autoOptimize.managed": "true"
   }
 )
 def sales_order_in_chicago():
-  return dlt.read_stream("sales_orders_cleaned").where("city == 'Chicago'")
+  df = dlt.read_stream("sales_orders_cleaned").where("city == 'Chicago'") 
+  df = df.select(df.city, df.order_date, df.customer_id, df.customer_name, explode(df.ordered_products).alias("ordered_products_explode"))
+
+  dfAgg = df.groupBy(df.order_date, df.city, df.customer_id, df.customer_name, df.ordered_products_explode.curr.alias("currency"))\
+    .agg(sum(df.ordered_products_explode.price).alias("sales"), sum(df.ordered_products_explode.qty).alias("qantity"))
+
+  return dfAgg
