7c4b2326181d1ab5982c53cf2967787ae9e8c0bf
Morgan Mazouchi <mojgan.mazouchi@databricks.com>
2024-05-31 12:54:58 -0700

    pattern 1 updated by adding source

--- cdc-from-snapshot-demo/single_full_snapshots/notebooks/Process_Snapshot - Pattern 1.py
+++ cdc-from-snapshot-demo/single_full_snapshots/notebooks/Process_Snapshot - Pattern 1.py
@@ -9,18 +9,17 @@ table = "orders_snapshot"
 table_name = f"{database_name}.{table}"
 snapshot_source_table_name = f"{database_name}.orders_snapshot"
 
-def get_current_timestamp_millis():
-  import datetime
-  current_datetime = datetime.datetime.now()
-  millis = int(current_datetime.timestamp())
-  return(millis)
+@dlt.view(name="source")
+def source():
+  return spark.read.table(snapshot_source_table_name)
+
 
 dlt.create_streaming_table(
   name = "orders"
   )
 dlt.apply_changes_from_snapshot(
   target = "orders",
-  snapshot_and_version = (spark.read.table(snapshot_source_table_name), get_current_timestamp_millis()),
+  source="source",
   keys = ["order_id"],
   stored_as_scd_type = 1
 )
